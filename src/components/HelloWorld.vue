a5a129257925fe490285a8b8086a45f7b886d3af
Joseph Luk <joseph.sc.luk@gmail.com>
2021-12-08 09:14:59 -0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import "leaflet/dist/leaflet.css";
+import { LMap, LMarker, LIcon } from "@vue-leaflet/vue-leaflet";
+
+let visible = ref(true);
+const toggle = function () {
+  visible.value = !visible.value;
+};
+</script>
+
+<template>
+  <l-map
+    ref="map"
+    :center="[0, 0]"
+    :zoom="2"
+    class="map"
+    style="width: 400px; height: 400px"
+  >
+    <template v-if="visible">
+      <l-marker :lat-lng="[0, 0]">
+        <l-icon>#</l-icon>
+      </l-marker>
+    </template>
+  </l-map>
+
+  <br />
+  <button @click="toggle">Toggle Marker</button>
+</template>
